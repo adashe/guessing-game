ed18736b3823913062e4ff0bbdfa611cab31e665
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-01-29 20:58:12 +0000

    Generates random number and asks for user guess

--- game.py
+++ game.py
@@ -2,5 +2,18 @@
 
 # Put your code here
 
+import random
+
+
+
 user_name = input("What's your name?")
 print(f'Hello {user_name}, welcome to our guessing game')
+
+random_number = random.randint(1, 101)
+
+print('Do not tell anyone, but the number is ' + str(random_number))
+
+user_guess = (input("Guess a number between 1 and 100."))
+
+print('Your guess is ' + str(user_guess))
+
